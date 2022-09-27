ba1c5ee49cdabeeb3436586a50c53ca0553eca78
angela213 <u19198958@tuks.co.za>
2022-09-27 09:12:11 +0200

    9:09 home

---
+++ JS/home.js
@@ -0,0 +1,80 @@
+//document.addEventListener("DOMContentLoaded", function() {
+
+function Tertius() {
+  document.getElementById("headingTeamMember").innerHTML = "Tertius";
+  document.getElementById("paragraphTeamMember").innerHTML = "Tertius is Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum ;alskjf;laskdjflaskjdlsakjfdkjkdjffffffffffffffffflskdjflaksjdflaksdjflaksdfasdfasdfasasdfakjsdfkahdfklajdfhlkajsdfh ajklsdfhlakd akljdhfalk sjdfha askjdfhklasjdfh ahklsjdfhlak haskdjfhaksjdfhakjd aksdjfhskj jdjdjd kajsdfhlakjsdfhklasjdfhal kajsdfhasdf";
+  document.getElementById("Tertius").style.filter = "brightness(120%)";
+
+  document.getElementById("Francois").style.filter = "brightness(100%)";
+  document.getElementById("Angela").style.filter = "brightness(100%)";
+  document.getElementById("Jay").style.filter = "brightness(100%)";
+  document.getElementById("James").style.filter = "brightness(100%)";
+}
+function Francois() {
+  document.getElementById("headingTeamMember").innerHTML = "Francois";
+  document.getElementById("paragraphTeamMember").innerHTML = "Francois is Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum ;alskjf;laskdjflaskjdlsakjfdkjkdjffffffffffffffffflskdjflaksjdflaksdjflaksdfasdfasdfasasdfakjsdfkahdfklajdfhlkajsdfh ajklsdfhlakd akljdhfalk sjdfha askjdfhklasjdfh ahklsjdfhlak haskdjfhaksjdfhakjd aksdjfhskj jdjdjd kajsdfhlakjsdfhklasjdfhal kajsdfhasdf";
+  document.getElementById("Francois").style.filter = "brightness(120%)";
+
+   document.getElementById("Tertius").style.filter = "brightness(100%)";
+  document.getElementById("Angela").style.filter = "brightness(100%)";
+  document.getElementById("Jay").style.filter = "brightness(100%)";
+  document.getElementById("James").style.filter = "brightness(100%)";
+}
+function Angela() {
+  document.getElementById("headingTeamMember").innerHTML = "Angela";
+  document.getElementById("paragraphTeamMember").innerHTML = "Angela is Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum ;alskjf;laskdjflaskjdlsakjfdkjkdjffffffffffffffffflskdjflaksjdflaksdjflaksdfasdfasdfasasdfakjsdfkahdfklajdfhlkajsdfh ajklsdfhlakd akljdhfalk sjdfha askjdfhklasjdfh ahklsjdfhlak haskdjfhaksjdfhakjd aksdjfhskj jdjdjd kajsdfhlakjsdfhklasjdfhal kajsdfhasdf";
+ document.getElementById("Angela").style.filter = "brightness(120%)";
+
+document.getElementById("Tertius").style.filter = "brightness(100%)";
+  document.getElementById("Francois").style.filter = "brightness(100%)";
+  document.getElementById("Jay").style.filter = "brightness(100%)";
+  document.getElementById("James").style.filter = "brightness(100%)";
+}
+function Jay() {
+  document.getElementById("headingTeamMember").innerHTML = "Jay";
+  document.getElementById("paragraphTeamMember").innerHTML = "Jay is Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum ;alskjf;laskdjflaskjdlsakjfdkjkdjffffffffffffffffflskdjflaksjdflaksdjflaksdfasdfasdfasasdfakjsdfkahdfklajdfhlkajsdfh ajklsdfhlakd akljdhfalk sjdfha askjdfhklasjdfh ahklsjdfhlak haskdjfhaksjdfhakjd aksdjfhskj jdjdjd kajsdfhlakjsdfhklasjdfhal kajsdfhasdf";
+ document.getElementById("Jay").style.filter = "brightness(120%)";
+
+ document.getElementById("Tertius").style.filter = "brightness(100%)";
+  document.getElementById("Francois").style.filter = "brightness(100%)";
+  document.getElementById("Angela").style.filter = "brightness(100%)";
+  document.getElementById("James").style.filter = "brightness(100%)";
+}
+function James() {
+  document.getElementById("headingTeamMember").innerHTML = "James";
+  document.getElementById("paragraphTeamMember").innerHTML = "James is Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum ;alskjf;laskdjflaskjdlsakjfdkjkdjffffffffffffffffflskdjflaksjdflaksdjflaksdfasdfasdfasasdfakjsdfkahdfklajdfhlkajsdfh ajklsdfhlakd akljdhfalk sjdfha askjdfhklasjdfh ahklsjdfhlak haskdjfhaksjdfhakjd aksdjfhskj jdjdjd kajsdfhlakjsdfhklasjdfhal kajsdfhasdf";
+ document.getElementById("James").style.filter = "brightness(120%)";
+
+ document.getElementById("Tertius").style.filter = "brightness(100%)";
+  document.getElementById("Francois").style.filter = "brightness(100%)";
+  document.getElementById("Angela").style.filter = "brightness(100%)";
+  document.getElementById("Jay").style.filter = "brightness(100%)";
+}
+
+  var light = document.getElementById("Light"),
+  lightBox = light.getBoundingClientRect(),
+  centerPoint = window.getComputedStyle(light).transformOrigin, //getting the transform origin of the light which we set in css
+  centers = centerPoint.split(" "); //splitting into x and y
+
+  function rotatelight(e) {
+     var lightEvent = e;
+     mouseX = e.clientX,
+     mouseY = e.clientY;
+
+   var centerY = lightBox.top + parseInt(centers[1]) - window.pageYOffset, //- window.pageYOffset,
+   centerX = lightBox.left + parseInt(centers[0])- window.pageXOffset,// - window.pageXOffset,
+   radians = Math.atan2(mouseX - centerX, (mouseY - centerY)),//(mouseX - centerX, mouseY - centerY),
+   degrees = (radians * (180 / Math.PI) * -1) + 180; 
+   degrees=degrees-205; //doesn't work
+   //console.log(degrees);
+   light.style.transform = 'rotate('+degrees+'deg)';
+
+  }
+const meetTeam=document.getElementById("meetTeam");
+  meetTeam.addEventListener('mousemove', rotatelight);
+
+
+
+
+
+

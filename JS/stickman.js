d18834f8c60514e6aecc6e89c39087467d27189b
TertiusDeJongh <70260891+TertiusDeJongh@users.noreply.github.com>
2022-10-26 15:54:53 +0200

    changed css and fonts

---
+++ JS/stickman.js
@@ -0,0 +1,10 @@
+document.getElementById("logoGif").style.display="none";
+function backflip(e){
+    e.style.display="none";
+    document.getElementById("logoGif").style.display="block";
+    setTimeout(resetLogo, 3400);
+}
+function resetLogo(){
+    document.getElementById("logoPic").style.display="block";
+    document.getElementById("logoGif").style.display="none";
+}

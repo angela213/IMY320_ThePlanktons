582dcc481662c511741633f7fcc2842a85c898b4
Francois Viviers <henryvivierz@gmail.com>
2022-10-27 21:42:57 +0200

    Added call to action page

--- JS/home.js
+++ JS/home.js
@@ -6,7 +6,7 @@ setTimeout(getHomePage, 7500);//starts counting from loaded
 function getHomePage(){//we nee to make this thing wait or make the loading generic enough that it doesn't have to
   console.log("hi");
     document.getElementById("loading").style.display="none";
-    window.location.href = "Pages/home.html";
+    window.location.href = "Pages/CallToAction.html";
 }//*/
 
 

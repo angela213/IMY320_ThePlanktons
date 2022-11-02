624097eb5e9195b9394adccce18372ad76606ca6
angela213 <u19198958@tuks.co.za>
2022-11-02 15:45:50 +0200

    yay

--- JS/loading.js
+++ JS/loading.js
@@ -1,6 +1,6 @@
-setTimeout(getHomePage, 7500);//starts counting from loaded
-function getHomePage(){//we nee to make this thing wait or make the loading generic enough that it doesn't have to
+setTimeout(getSplashPage, 7500);//starts counting from loaded
+function getSplashPage(){//we nee to make this thing wait or make the loading generic enough that it doesn't have to
   console.log("hi");
     document.getElementById("loading").style.display="none";
-    window.location.href = "Pages/home.html";
+    window.location.href = "Pages/SplashV3.html";
 }
